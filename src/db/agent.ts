b04faf17196497643df67eb6ba4066ffdf1efe68
Udaykumar Dhokia <udaykumardhokia@gmail.com>
2026-02-22 19:06:30 +0530

    project, postgres & drizzle setup

---
+++ src/db/agent.ts
@@ -0,0 +1,28 @@
+import {
+  uuid,
+  pgTable,
+  varchar,
+  boolean,
+  timestamp,
+  doublePrecision,
+} from "drizzle-orm/pg-core";
+
+export const agentsTable = pgTable("agents", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  desc: varchar("desc", { length: 500 }),
+  agent_id: varchar("agent_id", { length: 255 }).notNull().unique(),
+  is_active: boolean("is_active").default(false).notNull(),
+  version: varchar("version").notNull().default("0.0.1"),
+  base_url: varchar("base_url").notNull().unique(),
+  health_endpoint: varchar("health_endpoint").notNull(),
+  invoke_endpoint: varchar("invoke_endpoint").notNull(),
+  user_id: uuid("user_id").notNull(),
+  run_after_every_hours: doublePrecision().default(24.0).notNull(),
+  created_at: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updated_at: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
